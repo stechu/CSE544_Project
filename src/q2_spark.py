cef6501dd0efb38faa59ff2b9ce289421bd0dd68
Shumo Chu <shumo.chu@gmail.com>
2015-03-13 16:25:24 -0700

    update q2 spark code.

--- src/q2_spark.py
+++ src/q2_spark.py
@@ -1,4 +1,5 @@
 from pyspark import SparkContext
+import csv
 
 twitter_bucket = """s3n://bdbenchmark-data/twitter_2m.csv"""
 
@@ -14,7 +15,15 @@ if __name__ == "__main__":
         lambda x: (int(x[0]), int(x[1])))
 
     degrees = twitter.map(lambda x, y: (x, 1)).reduceByKey(lambda a, b: a+b)
-    remained = degrees.filter(lambda v, c: True if c >= 10 else False)
+    remained = degrees.filter(lambda v, c: True if c >= k else False)
 
-    twitter = twitter.join(remained)
-    print twitter.take(1)
+    while degrees.count() > remained.count():
+        twitter = twitter.join(remained).map(
+            lambda (v, (u, d)): (v, u)).coalesce(parallism)
+        degrees = twitter.map(
+            lambda x, y: (x, 1)).reduceByKey(lambda a, b: a+b)
+        remained = degrees.filter(lambda v, c: True if c >= k else False)
+
+    with open("twitter_10core.csv", 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(twitter)
