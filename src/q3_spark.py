dfeb9546cc8057327340a67376bb72ebf732a4ed
Shumo Chu <shumo.chu@gmail.com>
2015-03-15 00:17:47 -0700

    edges.

--- src/q3_spark.py
+++ src/q3_spark.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         lambda x: (long(x[0]), long(x[1]), float(x[2]),
                    float(x[3]), str(x[4]), long(x[5]), long(x[6])))
 
-    halo = sc.textFile(poi_bucket, parallism)
+    halo = sc.textFile(halo_bucket, parallism)
     halo = halo.map(lambda x: x.split(",")).map(
         lambda x: (long(x[0]), long(x[1]), long(x[2]), float(x[3]),
                    long(x[4]), float(x[5])))
@@ -35,7 +35,21 @@ if __name__ == "__main__":
     halo = halo.map(lambda x: ((x[0], x[1], x[2]), (x[3], x[4], x[5])))
     # after transformation: (nowGrp, grpId, timeStamp), (iOrder)
     poi = poi.map(lambda x: ((x[0], x[6], x[5]), (x[1])))
+    # after: (nowGrp, timeStamp, iOrder), grpId
     particles = poi.join(halo).map(
-        lambda (x, (a, b)): (x, b))
-    print particles.take(1)
+        lambda (x, (a, b)): ((x[0], x[2], a), x[1])).coalesce(parallism)
+    particles_add_1 = particles.map(
+        lambda ((x1, x2, x3), y): ((x1, x2+1, x3), y))
+
+    # join particles to edges
+    edgeInit = particles.join(particles_add_1).map(
+        lambda ((x1, x2, x3), (ng, cg)): ((x1, x2-1, ng, cg), 1))
+    # aggregate edges
+    edgeAgg = edgeInit.reduceByKey(lambda a, b: a + b)
+    # filter edges
+    edges = edgeAgg.filter(lambda (a, b): True if b > 256 else False)
+
+    print edges.take(1)
+
+
 
